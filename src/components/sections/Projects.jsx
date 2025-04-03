e1256f474a9d272e2b31ca09070afeb412f4b87a
Andres Vazquez <andres.vazquez.martin@gmail.com>
2025-04-03 23:58:39 +0200

    Projects added

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -40,6 +40,60 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Marvel React Library </h3>
+              <p className="text-gray-400 mb-4">
+               Official Marvel API used to check Marvel comics from chosen character.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React",  "CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://marvel-comic-library-react.netlify.app/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2"> Magic Match game</h3>
+              <p className="text-gray-400 mb-4">
+                Match game with image cards. 
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React",  "CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://heartfelt-fox-7a88d2.netlify.app"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
             <div
               className="
               glass p-6 rounded-xl border border-white/10 
